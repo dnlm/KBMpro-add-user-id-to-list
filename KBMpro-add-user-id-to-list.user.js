44289f65deb09f55bc609b961fb236dc24249bdd
Daniel Mann <daniel.mann@concept-design.de>
2018-06-04 14:58:46 +0200

    fix for custom icon positions

--- KBMpro-add-user-id-to-list.user.js
+++ KBMpro-add-user-id-to-list.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KBMpro Benutzerliste mit IDs
 // @namespace    http://www.concept-design.de/
-// @version      0.1
+// @version      0.2
 // @description  Add user id to KBMpro user list
 // @author       Daniel Mann
 // @match        https://cundd.kbmpro.de/kbmpro/index.php?action=user
@@ -11,7 +11,7 @@
 (function() {
     jQuery('.tabelle_allg .tr_gerade, .tabelle_allg .tr_ungerade').each(function () {
         jQuery('.spalte-aktionen nobr', jQuery(this)).prepend(
-            jQuery('.spalte-aktionen a:first-child', jQuery(this)).attr('href').split('=').reverse()[0] + "&nbsp;"
+            jQuery('.spalte-aktionen .fa-pencil', jQuery(this)).closest('a').attr('href').split('=').reverse()[0] + "&nbsp;"
         );
-    })
+    });
 })();
